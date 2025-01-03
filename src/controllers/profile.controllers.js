6d7fad3f64bb9464064b1641b289c8dfe49ae838
mrshivsharma <shivps7568@gmail.com>
2025-01-03 15:05:30 +0530

    feat: add profile update functionality and avatar/cover image upload routes

--- src/controllers/profile.controllers.js
+++ src/controllers/profile.controllers.js
@@ -4,7 +4,6 @@ import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadFileToCloudinary } from "../utils/cloudinary.js";
-import mongoose from "mongoose";
 
 const createProfile = asyncHandler(async (req, res) => {
   const { fullname, location, hobbies, bio, link, socialMedia } = req.body;
@@ -67,4 +66,133 @@ const getProfile = asyncHandler(async (req, res) => {
     .json(new apiResponse(200, profile, "Profile fetched successfully."));
 });
 
-export { createProfile, getProfile };
+// Update a profile
+const updateProfile = asyncHandler(async (req, res) => {
+  const { fullname, location, hobbies, bio, link, socialMedia } = req.body;
+
+  // Ensure required fields are provided
+  if (
+    [fullname, location, hobbies, bio, link, socialMedia].some(
+      (field) => !field?.trim()
+    )
+  ) {
+    throw new apiError(422, "Please fill in all the required fields");
+  }
+
+  const update = {
+    fullname,
+    location,
+    hobbies,
+    bio,
+    link,
+    socialMedia,
+  };
+
+  const profile = await Profile.findOneAndUpdate(
+    { username: req.user.username },
+    update,
+    {
+      new: true, // Return the updated document
+    }
+  );
+
+  if (!profile) {
+    throw new apiError(500, "Failed to update the profile");
+  }
+
+  return res.status(200).json(
+    new apiResponse(
+      200,
+      {
+        fullname: profile.fullname,
+        location: profile.location,
+        hobbies: profile.hobbies,
+        bio: profile.bio,
+        link: profile.link,
+        socialMedia: profile.socialMedia,
+      },
+      "Profile updated successfully."
+    )
+  );
+});
+
+// Update User Avatar
+const updateUserAvatar = asyncHandler(async (req, res) => {
+  const avatarLocalPath = req.file?.path;
+
+  if (!avatarLocalPath) {
+    throw new apiError(404, "Avatar file is missing");
+  }
+
+  const user = await Profile.findOne({ user: req.user._id });
+
+  if (!user) {
+    throw new apiError(404, "User not found");
+  }
+
+  const oldAvatarUrl = user.avatar;
+
+  const newAvatar = await uploadFileToCloudinary(avatarLocalPath, oldAvatarUrl);
+
+  if (!newAvatar.url) {
+    throw new apiError(400, "Error while uploading avatar");
+  }
+
+  const updatedUser = await Profile.findOneAndUpdate(
+    { user: req.user._id },
+    { $set: { avatar: newAvatar.url } },
+    { new: true }
+  );
+
+  return res
+    .status(200)
+    .json(
+      new apiResponse(200, updatedUser, "Avatar image updated successfully")
+    );
+});
+
+// Update User Cover Image
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+  const coverImageLocalPath = req.file?.path;
+
+  if (!coverImageLocalPath) {
+    throw new apiError(404, "Cover image file is missing");
+  }
+
+  const user = await Profile.findById(req.profile._id);
+
+  if (!user) {
+    throw new apiError(404, "User not found");
+  }
+
+  const oldCoverImageUrl = user.coverImage;
+
+  const newCoverImage = await uploadFileToCloudinary(
+    coverImageLocalPath,
+    oldCoverImageUrl
+  );
+
+  if (!newCoverImage.url) {
+    throw new apiError(400, "Error while uploading cover image");
+  }
+
+  const updatedUser = await Profile.findByIdAndUpdate(
+    req.profile._id,
+    { $set: { coverImage: newCoverImage.url } },
+    { new: true }
+  );
+
+  return res
+    .status(200)
+    .json(
+      new apiResponse(200, updatedUser, "Cover image updated successfully")
+    );
+});
+
+export {
+  createProfile,
+  getProfile,
+  updateProfile,
+  updateUserAvatar,
+  updateUserCoverImage,
+};
--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,16 +1,17 @@
 import express from "express";
 import {
   createProfile,
-  // updateUserAvatar,
-  // updateUserCoverImage,
   getProfile,
+  updateProfile,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/profile.controllers.js";
 import verifyJWT from "../middlewares/auth.middlewares.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 
 const router = express.Router();
 
-router.use(verifyJWT); // Protect all routes
+router.use(verifyJWT);
 
 // Create or update a profile
 router.route("/").post(
@@ -30,12 +31,14 @@ router.route("/").post(
 // Get a specific profile
 router.route("/:username").get(getProfile);
 
-// router
-//   .route("/update-User-Avatar")
-//   .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/:username").patch(updateProfile);
 
-// router
-//   .route("/update-User-CoverImage")
-//   .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router
+  .route("/update-User-Avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/update-User-CoverImage")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 export default router;
