241d214561501d718ea4c657739eb52fd9f957c5
MR-SHIV-SHARMA <shivps7568@gmail.com>
2025-02-22 14:17:30 +0530

    #

--- src/utils/cookieOptions.js
+++ src/utils/cookieOptions.js
@@ -1,4 +1,16 @@
+// production
 export const cookieOptions = {
   httpOnly: true,
   secure: true,
+  sameSite: "strict",
+  path: "/",
 };
+
+// local development
+// export const cookieOptions = {
+//   httpOnly: false,
+//   secure: false,
+//   sameSite: "lax",
+//   path: "/",
+//   maxAge: 24 * 60 * 60 * 1000, // 24 hours
+// };
